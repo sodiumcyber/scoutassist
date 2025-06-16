ffb4026ce377d0d2b9ba6f845bdf345aff391331
Sodium Cyber <118181147+sodiumcyber@users.noreply.github.com>
2025-06-16 10:38:33 +0100

    Update install_scoutsuite.sh
    Correct date

--- install_scoutsuite.sh
+++ install_scoutsuite.sh
@@ -8,7 +8,7 @@
 # Ensure the script is run in a terminal with internet access.
 
 # Authored by: Campbell Murray
-# Date: 2023-10-01
+# Date: 2025-06-16
 # Version: 1.0
 # Sodium Cyber Ltd - www.sodiumcyber.com
 
